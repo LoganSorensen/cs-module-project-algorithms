9d221f01c7edf13dc055804d50a14b67cf201d92
Logan Sorensen <loganmsorensen@gmail.com>
2020-05-20 15:11:54 -0700

    product_of_all_other_numbers passing

--- product_of_all_other_numbers/product_of_all_other_numbers.py
+++ product_of_all_other_numbers/product_of_all_other_numbers.py
@@ -2,15 +2,39 @@
 Input: a List of integers
 Returns: a List of integers
 '''
-def product_of_all_other_numbers(arr):
-    # Your code here
 
-    pass
+
+def product_of_all_other_numbers(arr):
+    product = 1
+    zeroProduct = 1
+    # calculates the product of every element in the array
+    for i in arr:
+        # if an element is not equal to zero, multiply it
+        if i != 0:
+            product *= i
+            zeroProduct *= i
+        else:
+            product *= i
+            zeroProduct *= 1
+
+    newArr = []
+    # divides the product by each element in the array to find the product
+    # of every other element
+    for i in arr:
+        if i != 0:
+            newArr.append(product//i)
+        else:
+            newArr.append(zeroProduct)
+
+    # print(newArr)
+    return newArr
 
 
 if __name__ == '__main__':
     # Use the main function to test your implementation
-    # arr = [1, 2, 3, 4, 5]
-    arr = [2, 6, 9, 8, 2, 2, 9, 10, 7, 4, 7, 1, 9, 5, 9, 1, 8, 1, 8, 6, 2, 6, 4, 8, 9, 5, 4, 9, 10, 3, 9, 1, 9, 2, 6, 8, 5, 5, 4, 7, 7, 5, 8, 1, 6, 5, 1, 7, 7, 8]
+    arr = [1, 2, 3, 4, 5]
+    # arr = [7, 9, 1, 8, 6, 0, 7, 8, 8, 7, 10]
+    # arr = [2, 6, 9, 8, 2, 2, 9, 10, 7, 4, 7, 1, 9, 5, 9, 1, 8, 1, 8, 6, 2, 6, 4, 8, 9, 5, 4, 9, 10, 3, 9, 1, 9, 2, 6, 8, 5, 5, 4, 7, 7, 5, 8, 1, 6, 5, 1, 7, 7, 8]
 
-    print(f"Output of product_of_all_other_numbers: {product_of_all_other_numbers(arr)}")
+    print(
+        f"Output of product_of_all_other_numbers: {product_of_all_other_numbers(arr)}")
