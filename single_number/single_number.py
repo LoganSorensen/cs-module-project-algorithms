3fe353f5e19de01261192e6feecb347a0a9c9ed2
Logan Sorensen <loganmsorensen@gmail.com>
2020-05-20 13:44:29 -0700

    single_number passing

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -3,9 +3,15 @@ Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
 def single_number(arr):
-    # Your code here
+    for i in range(len(arr)):
+        found_twice = False
 
-    pass
+        for j in range(len(arr)):
+            if i != j and arr[i] == arr[j]:
+                found_twice = True
+        if not found_twice:
+            return arr[i]
+    return -1
 
 
 if __name__ == '__main__':
@@ -13,3 +19,4 @@ if __name__ == '__main__':
     arr = [1, 1, 4, 4, 5, 5, 3, 3, 9, 0, 0]
 
     print(f"The odd-number-out is {single_number(arr)}")
+
