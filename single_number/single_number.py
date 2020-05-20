181054a1bc60290767766d27a688c616ba0ddbf6
Logan Sorensen <loganmsorensen@gmail.com>
2020-05-20 14:34:12 -0700

    moving_zeroes passing

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -2,6 +2,8 @@
 Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
+
+
 def single_number(arr):
     for i in range(len(arr)):
         found_twice = False
@@ -19,4 +21,3 @@ if __name__ == '__main__':
     arr = [1, 1, 4, 4, 5, 5, 3, 3, 9, 0, 0]
 
     print(f"The odd-number-out is {single_number(arr)}")
-
